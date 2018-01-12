bb809a61f770f6fb1196f50aadbdce19b0f82054
James Curatalo <jfcurat@gmail.com>
2018-01-12 03:41:51 -0800

    Added Firebase Config and Vars
    added firebase config
    added database var
    added other vars that will save input from forms

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,21 @@
-//.js here
+// .js here
 console.log('app.js linked!');
+
+// initialize firebase
+var config = {
+    apiKey: "AIzaSyCTxNWrFMUqkE8dLF4HJkjqkQPRB2ZZCpQ",
+    authDomain: "train-scheduler-752d0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-752d0.firebaseio.com",
+    projectId: "train-scheduler-752d0",
+    storageBucket: "",
+    messagingSenderId: "836362156835"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = '';
+var destination = '';
+var firstTrainTime = '';
+var frequencyMins = '';
+
